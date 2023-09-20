10584002a4722f2494f7b25fec02d509f51ade97
Ryan Turnquist <rturnq@gmail.com>
2023-09-20 13:39:30 -0700

    fix: base path script in head or body when present (#77)

--- src/__tests__/fixtures/isomorphic-runtime-base-path/test.config.ts
+++ src/__tests__/fixtures/isomorphic-runtime-base-path/test.config.ts
@@ -1,7 +1,24 @@
 import type { Options } from "../../..";
+import assert from "assert";
+
+async function hasScript(pattern: RegExp) {
+  const headScripts = await page.locator("head > script");
+  const len = await headScripts.count();
+  for (let i = 0; i < len; i++) {
+    const script = await headScripts.nth(i).innerHTML();
+    if (pattern.test(script)) {
+      return true;
+    }
+  }
+  return false;
+}
 
 export const ssr = true;
 export async function steps() {
+  assert(
+    await hasScript(/\$mbp[a-z0-1-_\s]*=/i),
+    "Base path script not found in head"
+  );
   await page.click("#clickable");
 }
 export const options: Options = {
