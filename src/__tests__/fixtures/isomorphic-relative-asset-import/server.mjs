efd00dc5246d93ab21044ae1963134a3ba719e81
Ryan Turnquist <rturnq@gmail.com>
2024-04-04 10:43:34 -0700

    fix: inconsistent test

--- src/__tests__/fixtures/isomorphic-relative-asset-import/server.mjs
+++ src/__tests__/fixtures/isomorphic-relative-asset-import/server.mjs
@@ -3,10 +3,7 @@ import path from "path";
 import url from "url";
 import { createServer } from "http";
 import serve from "serve-handler";
-
-globalThis.assetsPath = "/my-prefix/";
-// dyanmic import so globalThis.assetsPath can be set prior to the imported code executing
-const { handler } = await import("./dist/index.mjs");
+import { handler } from "./dist/index.mjs";
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const serveOpts = { public: path.resolve(__dirname, "dist") };
